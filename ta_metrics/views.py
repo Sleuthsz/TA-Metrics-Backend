a1094923989d5139f5e6e8bf8db3311b0bd4ab0c
Monika <monidavies@gmail.com>
2023-03-08 21:23:36 -0800

    Basic api call

--- ta_metrics/views.py
+++ ta_metrics/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+import environ
+import requests
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), 'config', '.env')
+load_dotenv(dotenv_path)
+#env = environ.Env()
+# environ.Env.read_env(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '../ta_metrics_project/.env'))
+
 
 # Create your views here.
+def call_api(num):
+    # base_url = env.str('CF_API')
+    base_url = os.environ.get('CF_API')
+    url = base_url + str(num)
+
+    res = requests.get(url)
+
+    return res.json()
+
+
+if __name__ == "__main__":
+    print(call_api(1))
--- ta_metrics_project/settings.py
+++ ta_metrics_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -18,11 +19,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+env = environ.Env(
+	 	DEBUG=(bool, False)
+	)
+environ.Env.read_env()
+
+
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a8=jb#@i+zigocnrk$2(++00#-@8ve10zolxa8(**z*jazb46f'
+SECRET_KEY = env.str('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = []
 
